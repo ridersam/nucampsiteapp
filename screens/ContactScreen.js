6b45f04b74004b6a5ce35537ed4f29372fb53ca1
Sam Rider <ridersam1023@gmail.com>
2023-09-16 10:02:33 -0500

    Workshop 1 - Task 2

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,8 +1,19 @@
 import { ScrollView } from "react-native-gesture-handler"
+import { Card, Text } from "react-native-elements";
 
 const ContactScreen = () => {
     return(
-        <ScrollView></ScrollView>
+        <ScrollView>
+            <Card wrapperStyle={{margin: 20}} >
+                <Card.Title>Contact information</Card.Title>
+                <Card.Divider />
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text style={{marginBottom: 10}}>U.S.A</Text>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>
+            </Card>
+        </ScrollView>
     )
 };
 
