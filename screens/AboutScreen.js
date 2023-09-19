0ee59e1637c9bd9bf00e81d7ba5871aa3b66e98c
Sam Rider <ridersam1023@gmail.com>
2023-09-19 14:08:24 -0500

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,14 +2,15 @@ import { Card, ListItem, Avatar } from "react-native-elements";
 import { FlatList, Text, ScrollView } from "react-native";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/loading Component";
 
 function Mission() {
     return (
         <Card>
             <Card.Title>Our Mission</Card.Title>
-            <Card.Divider/>
+            <Card.Divider />
             <Text style={{ margin: 10 }}>
-            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
             </Text>
         </Card>
     );
@@ -18,12 +19,37 @@ function Mission() {
 const AboutScreen = () => {
     const partners = useSelector((state) => state.partners);
 
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    < Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+
     return (
         <ScrollView>
-            <Mission/>
+            <Mission />
             <Card>
                 <Card.Title>Community Partners</Card.Title>
-                <Card.Divider/>
+                <Card.Divider />
                 {partners.partnersArray.map((partner) => (
                     <ListItem key={partner.id}>
                         <Avatar rounded source={{ uri: baseUrl + partner.image }} />
